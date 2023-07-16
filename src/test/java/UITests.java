d6f19ffc2ce92851c53e293825b708bbbc8ca3b0
Mirt12 <tanyabalashevich@gmail.com>
2023-07-16 18:03:54 +0200

    refactoring of UITests class

--- src/test/java/UITests.java
+++ src/test/java/UITests.java
@@ -1,3 +1,12 @@
-public class UITests extends BaseTest{
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
+public class UITests extends BaseTest {
+
+    @Test
+    public void testLoginWithIncorrectData() {
+        Page page = new Page(driver);
+        page.getURL().clickEnterBtn().fillEmailField("test@test.com").fillPwdField("password1").clickSubmitLoginFormBtn();
+        Assert.assertEquals(page.getErrorMessage(), "Неверно указан E-mail или пароль");
+    }
 }
