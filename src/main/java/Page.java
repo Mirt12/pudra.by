589e6ce1ae2813dfc51d985a7c439fe6f8cbb3aa
Mirt12 <tanyabalashevich@gmail.com>
2023-07-16 14:27:45 +0200

    initial commit

---
+++ src/main/java/Page.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+
+public class Page {
+    private WebDriver driver;
+
+    public Page(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    private String baseUrl;
+}
