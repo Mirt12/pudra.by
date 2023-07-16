cdbe08a552d1091456ae4af461b1f4165b695139
Mirt12 <tanyabalashevich@gmail.com>
2023-07-16 22:20:03 +0200

    add RestTest class

---
+++ src/test/java/RestTest.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class RestTest {
+    @Test
+    public void testlLoginWithInvalidData(){
+        RestPageObject po = new RestPageObject();
+        given().headers(po.getHeader()).formParams(po.getFormData()).
+                when().post(po.endPoint).
+                then().assertThat().statusCode(200).
+                body("code", equalTo(1002)).
+                body("message", equalTo("ERROR"));
+    }
+}
