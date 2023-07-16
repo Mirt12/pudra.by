4c7e842c874794e25d85cbb622401480a0b333e7
Mirt12 <tanyabalashevich@gmail.com>
2023-07-16 17:38:56 +0200

    refactoring of BaseTest class

--- src/main/java/BaseTest.java
+++ src/main/java/BaseTest.java
@@ -1,18 +1,25 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 
 import java.time.Duration;
 
 public class BaseTest {
     WebDriver driver;
 
-    @BeforeTest
+    @BeforeMethod
     public void WarmUp() {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(chromeOptions);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+
+    @AfterMethod
+    public void tearsDown() {
+        driver.quit();
     }
 }
--- src/test/java/UITests.java
+++ src/test/java/UITests.java
@@ -1,3 +1,3 @@
-public class UITests {
+public class UITests extends BaseTest{
 
 }
