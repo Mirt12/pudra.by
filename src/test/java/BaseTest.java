4a91262a36de4a383a3220fdfb9e1fd9d7b2f6f5
Mirt12 <tanyabalashevich@gmail.com>
2023-07-18 19:40:12 +0200

    refactored imports

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,6 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
-
-import java.time.Duration;
 
 public class BaseTest {
 
